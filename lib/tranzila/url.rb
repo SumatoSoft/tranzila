ea8dd4389cc6fc4a7f0853e16dcc5bf0df39e6e9
Yuri Zubov <y.zubov@sumatosoft.com>
2020-04-01 06:58:44 +0300

    Adds ability to change terminal for generation link

--- lib/tranzila/url.rb
+++ lib/tranzila/url.rb
@@ -1,11 +1,12 @@
 module Tranzila
   class Url
-    def initialize(params = {})
+    def initialize(params = {}, supplier_terminal = nil)
       @params = params
+      @supplier_terminal = supplier_terminal || Tranzila.configuration.supplier
     end
 
     def call
-      "https://direct.tranzila.com/#{Tranzila.configuration.supplier}/iframe.php?#{@params.to_param}"
+      "https://direct.tranzila.com/#{@supplier_terminal}/iframe.php?#{@params.to_param}"
     end
   end
 end
